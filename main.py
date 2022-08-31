417fe5ea1f111f27380eb67ca29a74f5f8dd927a
L91012758 <sonygion@gmail.com>
2022-08-31 18:41:02 +0200

    ggg

---
+++ main.py
@@ -0,0 +1,31 @@
+import sys
+
+from PyQt5.QtWidgets import (
+    QApplication,
+    QMainWindow,
+)
+
+from funzioni_tab import mainfunctions
+from sqlHelper import createConnection
+from untitled import Ui_MainWindow
+
+
+class Contacts(QMainWindow, Ui_MainWindow, mainfunctions):
+    def __init__(self, *args, obj=None, **kwargs):
+        super(Contacts, self).__init__(*args, **kwargs)
+        self.setupUi(self)
+        self.main_tab.setCurrentIndex(0)
+        self.main_tab.currentChanged.connect(self.prova)
+        self.listView_Gruppi.clicked.connect(self.clicked_gruppi_item)
+
+    def clicked_gruppi_item(self, i):
+
+        it = self.model.itemFromIndex(i)
+        print(" "+"cliccato: "+str(it.text()))
+
+app = QApplication(sys.argv)
+if not createConnection():
+    sys.exit(1)
+win = Contacts()
+win.show()
+sys.exit(app.exec_())
